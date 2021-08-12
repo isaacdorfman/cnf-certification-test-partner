df52d96e30d6566b2f37227bb51575ae345c4a54
Isaac Dorfman <idorfman@redhat.com>
2021-08-12 18:16:24 +0300

    CNFCERT-30 changes

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 
 install: 
 	bash ./deploy-all.sh
+
+install-partner-pods:
+	bash deploy-partner-pods.sh
 	
 clean:
 	bash ./clean-all.sh
--- deploy-all.sh
+++ deploy-all.sh
@@ -1,41 +1,16 @@
-mkdir -p ./temp
+bash deploy-partner-pods.sh
 
 if [[ -z "${NAMESPACE_TO_GENERATE}" ]]; then
     export NAMESPACE_TO_GENERATE="tnf"
 fi
 
-export res=$(oc get namespace $NAMESPACE_TO_GENERATE 2>&1)
-if [[ ${res#Error from server (NotFound):} != ${res} ]]; then
-    cat ./local-test-infra/namespace.yaml | ./script/mo > ./temp/rendered-namespace.yaml
-    oc apply -f ./temp/rendered-namespace.yaml
-    rm ./temp/rendered-namespace.yaml
-else
-    echo "namespace ${NAMESPACE_TO_GENERATE} already exists, no reason to recreate"
-fi
-
-export res=$(oc get pod -n $NAMESPACE_TO_GENERATE -l test-network-function.com/generic=fs_diff_master 2>&1)
-if [[ ${res#No resources found} != ${res} ]]; then
-    cat ./local-test-infra/fsdiff-pod.yaml | ./script/mo > ./temp/rendered-fsdiff-template.yaml
-    oc apply -f ./temp/rendered-fsdiff-template.yaml
-    rm ./temp/rendered-fsdiff-template.yaml
-else
-    echo "fsDiffMasterPod already exists, no reason to recreate"
-fi
-
-export res=$(oc get pod -n $NAMESPACE_TO_GENERATE -l test-network-function.com/generic=target 2>&1)
-if [[ ${res#No resources found} != ${res} ]]; then
+export res=$(oc get deployment -n $NAMESPACE_TO_GENERATE test 2>&1)
+if [[ ${res#Error from server (NotFound)} != ${res} ]] || [[ ${res#No resources found} != ${res} ]]; then
     cat ./local-test-infra/local-pod-under-test.yaml | ./script/mo > ./temp/rendered-local-pod-under-test-template.yaml
     oc apply -f ./temp/rendered-local-pod-under-test-template.yaml
     rm ./temp/rendered-local-pod-under-test-template.yaml
 else
-    echo "partner pod already exists, no reason to recreate"
+    echo "target pod already exists, no reason to recreate"
 fi
-
-export res=$(oc get pod -n $NAMESPACE_TO_GENERATE -l test-network-function.com/generic=orchestrator 2>&1)
-if [[ ${res#No resources found} != ${res} ]]; then
-    cat ./local-test-infra/local-partner-pod.yaml | ./script/mo > ./temp/rendered-partner-template.yaml
-    oc apply -f ./temp/rendered-partner-template.yaml
-    rm ./temp/rendered-partner-template.yaml
-else
-    echo "partner pod already exists, no reason to recreate"
-fi
+sleep 3
+oc wait pod -n $NAMESPACE_TO_GENERATE --for=condition=ready -l "app=test" --timeout=30s
